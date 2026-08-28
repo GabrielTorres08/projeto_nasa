91c91a978a1a44110748fd8cd3e1193a069f3527
Gabriel_dos_Santos_Torres <gabrielstorres00@gmail.com>
2026-08-28 15:34:03 -0300

    Melhoria na resposividade de vídeos, .env.example criado e melhoria no server.py

--- server.py
+++ server.py
@@ -1,24 +1,61 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
+
 def dados_API():
-  apod_url = "https://api.nasa.gov/planetary/apod"
-  response = requests.get(apod_url, params={"api_key": "1bwitvx0d1gqegHP2DeORpKbGRSaacChbEL6WeDy"})
-  data = response.json()
-  return data
+
+    apod_url = "https://api.nasa.gov/planetary/apod"
+
+    response = requests.get(
+        apod_url,
+        params={
+            "api_key": os.getenv("API_KEY")
+        }
+    )
+
+    response.raise_for_status()
+    return response.json()
+
+
+def transformar_video_url(url):
+    if "youtube.com/watch?v=" in url:
+        video_id = url.split("v=")[1].split("&")[0]
+        return f"https://www.youtube.com/embed/{video_id}"
+    
+    elif "youtu.be/" in url:
+        video_id = url.split("youtu.be/")[1].split("?")[0]
+        return f"https://www.youtube.com/embed/{video_id}"
+    
+    return url
 
 
 @app.route("/")
 def home():
-  dados = dados_API()
-  img_url = dados.get("url")
-  titulo = dados.get('title')
-  descricao = dados.get('explanation')
-  return render_template("index.html", img_url=img_url, titulo=titulo, descricao=descricao)
+
+    dados = dados_API()
+
+    titulo = dados.get("title")
+    descricao = dados.get("explanation")
+    media_url = dados.get("url")
+    media_type = dados.get("media_type")
+
+    if media_type == "video":
+        media_url = transformar_video_url(media_url)
+
+    return render_template(
+        "index.html",
+        titulo=titulo,
+        descricao=descricao,
+        media_url=media_url,
+        media_type=media_type
+    )
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
